c9502217fd08f4155f62845ac693a97a4cb5a743
rulingghost <gebcay@gmail.com>
2026-02-09 13:17:56 +0300

    Secure Firebase credentials using environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, set, update } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDocUSrZDoeLmsIISIjrZCbtwwCMAeyyGA",
-  authDomain: "gamadoroyyorum.firebaseapp.com",
-  databaseURL: "https://gamadoroyyorum-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "gamadoroyyorum",
-  storageBucket: "gamadoroyyorum.firebasestorage.app",
-  messagingSenderId: "425739407219",
-  appId: "1:425739407219:web:483287d080a16b368a3f06",
-  measurementId: "G-ZPQ7NB826E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
